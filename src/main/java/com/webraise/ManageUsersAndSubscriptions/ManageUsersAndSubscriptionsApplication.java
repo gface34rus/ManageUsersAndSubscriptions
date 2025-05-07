4d762707e6e1438e3ac70e24e9a6eff7904635bd
gface <89259112970@mail.ru>
2025-05-07 11:36:11 +0300

    Initial commit

---
+++ src/main/java/com/webraise/ManageUsersAndSubscriptions/ManageUsersAndSubscriptionsApplication.java
@@ -0,0 +1,12 @@
+package com.webraise.ManageUsersAndSubscriptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageUsersAndSubscriptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageUsersAndSubscriptionsApplication.class, args);
+    }
+}
