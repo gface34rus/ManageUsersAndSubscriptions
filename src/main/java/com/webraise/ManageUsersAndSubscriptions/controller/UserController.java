a98e17e622a8e523d4ce3d9f6dc8b99611a78355
gface <89259112970@mail.ru>
2025-05-08 10:02:39 +0300

    add SubscriptionController, UserController

---
+++ src/main/java/com/webraise/ManageUsersAndSubscriptions/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.webraise.ManageUsersAndSubscriptions.controller;
+
+import com.webraise.ManageUsersAndSubscriptions.model.User;
+import com.webraise.ManageUsersAndSubscriptions.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User updatedUser = user.get();
+            updatedUser.setName(userDetails.getName());
+            updatedUser.setEmail(userDetails.getEmail());
+            return ResponseEntity.ok(userRepository.save(updatedUser));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
